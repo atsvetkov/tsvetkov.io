b6d15c969643608fb6922026232ce971864147f6
Alexander Tsvetkov <iammamba@gmail.com>
2017-02-16 23:09:20 +0100

    new coffee and links post

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/atsvetkov/tsvetkov.io.git /var/www/tsvetkov.io/temp
-mkdir /var/www/tsvetkov.io/temp/themes
 git clone https://github.com/atsvetkov/hyde.git /var/www/tsvetkov.io/temp/themes/hyde
 ~/work/bin/hugo -s /var/www/tsvetkov.io/temp -d /var/www/tsvetkov.io/html
 rm -rf /var/www/tsvetkov.io/temp

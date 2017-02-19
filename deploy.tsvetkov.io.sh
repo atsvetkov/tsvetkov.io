ee259e8031734d14d81be0038b324c4293b4acad
Alexander Tsvetkov <iammamba@gmail.com>
2017-02-19 19:18:43 +0100

    fixed path

--- deploy.tsvetkov.io.sh
+++ deploy.tsvetkov.io.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/atsvetkov/tsvetkov.io.git /var/www/tsvetkov.io/temp
 git clone https://github.com/atsvetkov/hyde.git /var/www/tsvetkov.io/temp/themes/hyde
-~/work/bin/hugo --config=/var/www/tsvetkov.io/config.tsvetkov.io.toml -s /var/www/tsvetkov.io/temp -d /var/www/tsvetkov.io/html
+~/work/bin/hugo --config=/var/www/tsvetkov.io/temp/config.tsvetkov.io.toml -s /var/www/tsvetkov.io/temp -d /var/www/tsvetkov.io/html
 rm -rf /var/www/tsvetkov.io/temp
 echo done!

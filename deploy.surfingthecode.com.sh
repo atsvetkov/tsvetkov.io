fddddb9e518e180b66dd7fecf539caa4332a9aa8
Alexander Tsvetkov <iammamba@gmail.com>
2017-02-19 19:10:22 +0100

    modified

--- deploy.surfingthecode.com.sh
+++ deploy.surfingthecode.com.sh
@@ -2,5 +2,4 @@
 git clone https://github.com/atsvetkov/tsvetkov.io.git /var/www/surfingthecode.com/temp
 git clone https://github.com/atsvetkov/hyde.git /var/www/surfingthecode.com/temp/themes/hyde
 ~/work/bin/hugo --config=/var/www/surfingthecode.com/temp/config.surfingthecode.com.toml -s /var/www/surfingthecode.com/temp -d /var/www/surfingthecode.com/html
-rm -rf /var/www/surfingthecode.com/temp
 echo done!
